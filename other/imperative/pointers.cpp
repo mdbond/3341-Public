b5025d9c3ca79b7a642709925d5c86747771b209
Michael Bond <mikebond@cse.ohio-state.edu>
2020-09-14 10:39:38 -0400

    more printing

--- other/imperative/pointers.cpp
+++ other/imperative/pointers.cpp
@@ -14,15 +14,24 @@ int main() {
   cout << "&w = " << &w << endl; // It's important that &w be evaluated somewhere, so it gets a stack location!
   cout << "&x = " << &x << endl;
 
-  cout << "p = " << p << ", w = " << w << ", x = " << x << endl;
+  cout << "BEFORE MODIFICATIONS:      p = " << p << ", w = " << w << ", x = " << x << endl;
 
   *p = 4;
 
+  cout << "BEFORE FIRST MODIFICATION: p = " << p << ", w = " << w << ", x = " << x << endl;
+
   *(p + 1) = 5; // same as *((int*)((void*)p + 8)) = 5;
 
-  cout << "p = " << p << ", w = " << w << ", x = " << x << endl;
+  cout << "AFTER MODIFICATIONS:       p = " << p << ", w = " << w << ", x = " << x << endl;
 
   cout << "*p = " << *p << endl;
 
   return 0;
+
+  /*int z[5];
+  int* y = z;
+  y[2] = 5;
+  cout << y[2] << " " << (y+1)[1] << " " << *(y + 2) << endl;
+  cout << *(int*)((void*)y + sizeof(int)*2) << endl;*/
+
 }
